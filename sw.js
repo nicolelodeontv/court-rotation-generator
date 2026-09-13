da469e0cb96787052ec27caa3572bc2a4db6ee07
Nicole John Dela Cruz <njuandelacruz@gmail.com>
2026-09-13 17:10:00 +0800

    Remove Facility Plans from service-worker cache

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='crg-facility-v1';
-const ASSETS=['./','./index.html','./styles.css','./button-ui-fix.css','./pro-upgrades.css','./facility-upgrades.css','./scheduler.js','./shuffle-fix.js','./pro-bypass.js','./pro-upgrades.js','./pro-hotfix.js','./facility-upgrades.js','./app.js','./favicon.svg'];
+const CACHE='crg-clean-v2';
+const ASSETS=['./','./index.html','./styles.css','./scheduler.js','./app.js','./favicon.svg','./manifest.webmanifest'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(event.request,copy)).catch(()=>{});return res}).catch(()=>caches.match('./index.html'))))});
