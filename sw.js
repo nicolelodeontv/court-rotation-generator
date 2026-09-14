a708c08548737b4049aed0524f56c792c42ac2bc
Nicole John Dela Cruz <njuandelacruz@gmail.com>
2026-09-14 18:30:23 +0800

    Force refresh service worker cache for latest app

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='crg-clean-v2';
-const ASSETS=['./','./index.html','./styles.css','./scheduler.js','./app.js','./favicon.svg','./manifest.webmanifest'];
+const CACHE='crg-clean-v3';
+const ASSETS=['./','./index.html','./styles.css?v=crg37','./features.css?v=crg37','./scheduler.js?v=crg37','./app.js?v=crg37','./features.js?v=crg37','./favicon.svg','./manifest.webmanifest'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(event.request,copy)).catch(()=>{});return res}).catch(()=>caches.match('./index.html'))))});
